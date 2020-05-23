cbd4f7616053aaa3ad6214fab9b96114c3d15a73
huuanhhuynguyen <huuanhhuynguyen@gmail.com>
2020-05-23 17:00:50 +0200

    Add initial files

---
+++ src/my_robot/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ngh7rng/udacity_robotics/WhereAmI/src/my_robot
+# Build directory: /home/ngh7rng/udacity_robotics/WhereAmI/src/my_robot/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ src/my_robot/cmake-build-debug/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/ngh7rng/udacity_robotics/WhereAmI/src/my_robot/cmake-build-debug/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
